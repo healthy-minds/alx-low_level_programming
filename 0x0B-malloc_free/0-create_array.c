c602beca4a535d7e3eeb87a522b9663224469258
Fuufuu <ifunanyanwalusiuka@gmail.com>
2023-05-16 16:15:36 -0700

    0. Float like a butterfly, sting like a bee

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#iinclude "main.h"
+
+/**
+ * create_array - create an array of and initialize it with a specific value
+ * @size: the desired size of the array
+ * @c: the value with which the array should be initialized
+ *
+ * Return: NULL if size is 0 or memory allocation fails,
+ * otherwise 0
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+
+	if (!size)
+		return (NULL);
+
+	array = (char *) malloc(sizeof(char) * size);
+
+	if (array)
+	{
+		while (size > 0)
+			array[--size] = c;
+	}
+	return (array);
+}
