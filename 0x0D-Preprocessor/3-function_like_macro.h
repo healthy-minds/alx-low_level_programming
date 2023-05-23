4529a74c421d27f4c359b67daa07c19f67816a00
Fuufuu <ifunanyanwalusiuka@gmail.com>
2023-05-23 14:53:26 -0700

    MACRO

---
+++ 0x0D-Preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * File: 3-function_like_macro
+ * @X: input
+ * Desc: header file
+ */
+
+#define ABS(X) ((X) < 0 ? -(1) * (X) : (X)) 
+
+#endif
---
+++ 0x0D-Preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * File: 4-sum
+ * @x: input
+ * @y: input
+ * Desc: header file
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
